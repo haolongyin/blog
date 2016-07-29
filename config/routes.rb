788ed0d684b5a4c1f8c727d200dc16304e02425c
Yin Haolong <haolongyin@gmail.com>
2016-07-29 14:26:08 +0800

    Added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ get 'pages/blog'
 get 'pages/about'
 
 get 'blog' => 'pages#blog'
-
+get 'about' => 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
