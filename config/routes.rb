3f68014d9e3a22ff77c19657f679cafeea573789
Yin Haolong <haolongyin@gmail.com>
2016-07-29 13:49:43 +0800

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+root 'pages#welcome'
+
+  
+get 'pages/welcome'
+get 'pages/blog'
+get 'pages/about'
+
+get 'blog' => 'pages#blog'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
